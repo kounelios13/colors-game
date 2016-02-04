1af94ab9eb0de9014a6e70f988ca97ebee14fd09
Γιώργος Λυμπερόπουλος <glympe@me.com>
2016-02-04 16:47:30 +0200

    Almost done. Added material design look on divs and colors

--- colors.js
+++ colors.js
@@ -8,24 +8,79 @@ var rgbDisplay = document.getElementById("rgbDisplay");
 //User Message
 var messageDisplay = document.getElementById("message");
 var h1 = document.querySelector("h1");
+var resetBtn = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyMode");
+var hardBtn = document.querySelector("#hardMode");
+var isHard = true;
 
-//Adding colors to divs and click listeners
-for (i = 0; i < squares.length; i++ ) {
-	//Add colors to divs
-	squares[i].style.background = colors[i];
-	//Adding an click event listener to divs
-	squares[i].addEventListener("click", function(){
-		var clickedColor = this.style.background;
-		if (clickedColor === pickedColor) {
-			messageDisplay.textContent = "Correct";
-			changeColors(pickedColor);
-			h1.style.background = clickedColor
 
-		}else {
-			this.style.background = "#232323";
-			messageDisplay.textContent = "Try again!"
+
+	//Adding colors to divs and click listeners
+	for (i = 0; i < squares.length; i++ ) {
+		//Add colors to divs
+		squares[i].style.background = colors[i];
+		//Adding an click event listener to divs
+		squares[i].addEventListener("click", function(){
+			var clickedColor = this.style.background;
+			if (clickedColor === pickedColor) {
+				messageDisplay.textContent = "Correct!!";
+				resetBtn.textContent = "Play again?"
+				changeColors(pickedColor);
+				h1.style.background = clickedColor
+
+			}else {
+				this.style.background = "#f5f5f5";
+				messageDisplay.textContent = "Try again!"
+			}
+		});
+	}
+
+easyBtn.addEventListener("click", function(){
+	easyBtn.classList.add("selected");
+	hardBtn.classList.remove("selected");
+	isHard = false;
+	reset(3);
+});
+
+hardBtn.addEventListener("click", function(){
+	easyBtn.classList.remove("selected");
+	hardBtn.classList.add("selected");
+	isHard = true;
+	reset(6);
+});
+
+
+resetBtn.addEventListener("click", function(){
+	if (isHard) {
+		reset(6);
+	}else{
+		reset(3);
+	}
+});
+
+function reset(num){
+	colors = generateRandomColors(num);
+	pickedColor = pickColor();
+	rgbDisplay.textContent = pickedColor;
+	for (i = 0; i < squares.length; i++ ) {
+		//Add colors to divs
+		if(isHard){
+			squares[i].style.display = "block";
+			squares[i].style.background = colors[i];
+		}else{
+			if (i > 2){
+				squares[i].style.display = "none"
+			}else{
+
+				squares[i].style.background = colors[i];
+			}
 		}
-	});
+		
+	}
+	resetBtn.textContent = "New colors"
+	h1.style.background = "#3F51B5";
+	messageDisplay.textContent = "";
+
 }
 
 //Displaying the color which player needs to find
